10c35a5d83a9a53d21a74955bd78b6a158f3ec22
John Carr <john.carr@unrouted.co.uk>
2013-08-26 20:52:09 +0100

    Bundle certs in windows build

--- YaybuShell.py
+++ YaybuShell.py
@@ -1,38 +1,47 @@
 import sys
 import os
 
+code_dir = os.path.dirname(sys.path[0])
+
 
 # Make sure egg-info files are on the PYTHONPATH
-sys.path.append(os.path.dirname(sys.path[0]))
+sys.path.append(code_dir)
+
+
+# Bundle a cacert.pem so HTTPS is secure
+import libcloud.security
+libcloud.security.CA_CERTS_PATH.append(
+    os.path.join(code_dir, "cacert.pem")
+    )
 
 
 # Fix libcloud on windows
 import socket
 import ctypes
- 
+
 class sockaddr(ctypes.Structure):
     _fields_ = [("sa_family", ctypes.c_short),
                 ("__pad1", ctypes.c_ushort),
                 ("ipv4_addr", ctypes.c_byte * 4),
                 ("ipv6_addr", ctypes.c_byte * 16),
                 ("__pad2", ctypes.c_ulong)]
- 
+
 WSAStringToAddressA = ctypes.windll.ws2_32.WSAStringToAddressA
 WSAAddressToStringA = ctypes.windll.ws2_32.WSAAddressToStringA
- 
+
 def inet_pton(address_family, ip_string):
     addr = sockaddr()
     addr.sa_family = address_family
     addr_size = ctypes.c_int(ctypes.sizeof(addr))
- 
+
     if WSAStringToAddressA(ip_string, address_family, None, ctypes.byref(addr), ctypes.byref(addr_size)) != 0:
         raise socket.error(ctypes.FormatError())
- 
+
     if address_family == socket.AF_INET:
         return ctypes.string_at(addr.ipv4_addr, 4)
     if address_family == socket.AF_INET6:
         return ctypes.string_at(addr.ipv6_addr, 16)
- 
+
     raise socket.error('unknown address family')
 
 setattr(socket, "inet_pton", inet_pton)
