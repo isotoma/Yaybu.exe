73acd7db24416972cc61dbf48b65498d7b57f1c1
John Carr <john.carr@isotoma.com>
2014-03-07 14:46:56 +0000

    Avoid KeyError in windows builds

--- YaybuShell.py
+++ YaybuShell.py
@@ -1,33 +1,42 @@
-import sys
-import os
+def main():
+    from gevent.monkey import patch_all
+    patch_all(subprocess=True)
 
-code_dir = os.path.dirname(sys.path[0])
+    import sys
+    import os
 
-
-# Make sure egg-info files are on the PYTHONPATH
-sys.path.append(code_dir)
-
-
-# Requests bundles cacert.pem, but it can't see it inside library.zip
-# The bundler will ship cacert.pem alongside yaybu.exe
-import requests.certs
-requests.certs.where = lambda: os.path.join(code_dir, "cacert.pem")
+    code_dir = os.path.dirname(sys.path[0])
 
 
-# libcloud depends on a system cert bundle. Point it at the requests
-# cacert.pem...
-import libcloud.security
-libcloud.security.CA_CERTS_PATH.append(
-    os.path.join(code_dir, "cacert.pem")
-    )
+    # Make sure egg-info files are on the PYTHONPATH
+    sys.path.append(code_dir)
 
 
-# Make sure the yay generated files are included in the bundle
-from yay import lextab
-from yay import parsetab
+    # Requests bundles cacert.pem, but it can't see it inside library.zip
+    # The bundler will ship cacert.pem alongside yaybu.exe
+    import requests.certs
+    requests.certs.where = lambda: os.path.join(code_dir, "cacert.pem")
 
 
-from yaybu.core.main import main
+    # libcloud depends on a system cert bundle. Point it at the requests
+    # cacert.pem...
+    import libcloud.security
+    libcloud.security.CA_CERTS_PATH.append(
+        os.path.join(code_dir, "cacert.pem")
+        )
+
+
+    # Make sure the yay generated files are included in the bundle
+    from yay import lextab
+    from yay import parsetab
+
+
+    from yaybu.core.main import main
+    main()
+
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
